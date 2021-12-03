d3e298a6ac804453c30b634120c80bb12c930acd
Tim Honchel <tim.honchel@gmail.com>
2021-12-03 15:03:03 -0500

    complete

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the following information in order to create a story:");
+
+            Console.WriteLine();
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Favorite Color:");
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Favorite Animal:");
+            string favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Favorite Band:");
+            string favoriteBand = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(Story(name, favoriteColor, favoriteAnimal, favoriteBand));
+           
+        }
+
+        public static string Story(string name, string color, string animal, string band)
+        {
+            string story = "Once upon a time, there was a " + name + ". " + name + " had a thing for " + color + " "+ animal +"s. One day when " + name + " was at a concert for " + band + ", a " + color + " " + animal + " appeared.";
+            return story;
+
+        }
+    }
+}
